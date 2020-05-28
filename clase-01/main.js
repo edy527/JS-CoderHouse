ddb522da19eff7a6df5bfe64959133a4ade2bd8e
edy527 <edgardobeltran527@gmail.com>
2020-05-28 16:11:24 -0300

    Se agregan carpetas de clase 1 y clase 2

---
+++ clase-01/main.js
@@ -0,0 +1,17 @@
+/* Con los conocimientos vistos en la clase, crearás un script en JS que le solicite al usuario
+ ingresar datos, y luego mediante JavaScript realizarás operaciones sobre esos datos. 
+ 
+ */
+
+/* var nombre = prompt("Ingresá tu nombre");
+console.log("Hola " + nombre + "!");
+
+var edadAdmin = 28;
+var edadUsuario = parseInt(prompt("Por favor ingresá tu edad"));
+var resultadoSumaEdades = edadAdmin + edadUsuario;
+
+console.log("el resultado es: " + resultadoSumaEdades);
+alert(
+  "Tu edad y mi edad sumadas dan como resultado " + (edadUsuario + edadAdmin)
+);
+ */
---
+++ clase-02/main.js
@@ -0,0 +1,14 @@
+/* var numero = prompt("Ingresa un numero de cuatro digitos");
+if (numero > 1000) {
+  alert("El numero ingresado es mayor a 1000");
+}
+
+var saludo = prompt("Ingresa un saludo");
+if ((saludo = "Hola")) {
+  alert("Ingresaste el saludo correcto!");
+} */
+
+var numero2 = prompt("Ingresa un numero entre el 1 y el 50");
+if (numero2 >= 10 && numero2 <= 50) {
+  alert("Tu numero es ");
+}
